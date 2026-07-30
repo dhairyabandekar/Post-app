238984c3db01a1413e65eed50b6d2742d68217ef
dhairyabandekar <dhairyabandekar@gmail.com>
2026-07-30 14:04:30 +0530

    fix: update API base YRL for production deployment

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://post-app-ri4c.onrender.com",
 });
 
 api.interceptors.request.use(
